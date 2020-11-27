848dd3dfbc64f9785214c266b34028ad317dd8dd
Hyt <69683382@qq.com>
2020-11-27 16:27:08 +0800

    成功导入NetMQ并找到正确使用的基本方法
    各种资源的释放太折磨人了

--- Assets/Scripts/Tester.cs
+++ Assets/Scripts/Tester.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Threading;
+using System.Threading.Tasks;
 using IPC;
+using NetMQ;
+using NetMQ.Sockets;
 using UnityEngine;
 
 namespace MazeViewer
@@ -12,47 +15,58 @@ namespace MazeViewer
     {
         private Thread thread;
 
-        [ContextMenu("Kill Thread")]
-        public void KillThread()
-        {
-            thread?.Abort();
-        }
+        public string address;
+
+        //[ContextMenu("Close Socket")]
+        //public void CloseSocket()
+        //{
+        //    socket?.Close();
+        //    // call this to clean up everything
+        //    NetMQConfig.Cleanup();
+        //    task.Wait();
+        //}
+
         [ContextMenu("Test")]
         public void Test()
         {
-            Debug.Log("Test something");
-            //string a = "123 123";
-            //var callWatch = System.Diagnostics.Stopwatch.StartNew();
-            //Thread thread = new Thread(() =>
-            //{
-            //    var watch = System.Diagnostics.Stopwatch.StartNew();
-            //    for (int i = 0; i < 1e5; i++)
-            //    {
-            //        var l = new List<string>(a.Split(' '));
-            //    }
-            //    Debug.Log($"Time used:{watch.ElapsedMilliseconds}ms.");
-            //});
-            //thread.Start();
-            //thread?.Abort();
-            //thread = new Thread(() =>
-            //{
-            //    try
-            //    {
-            //        using(MazeEditorProxy proxy = new MazeEditorProxy())
-            //        {
-            //            proxy.SendFrameString("Hey! this msg is from unity!");
-            //            string msg = proxy.ReceiveFrameString();
-            //            UnityEngine.Debug.Log("Msg received is: \"" + msg + "\"");
-            //        }
-            //    }
-            //    catch(Exception e)
-            //    {
-            //        UnityEngine.Debug.LogError("error: " + e);
-            //        throw;
-            //    }
-            //});
-            //thread.Start();
-            
+            thread = new Thread(RunSocket);
+            thread.IsBackground = true;
+            thread.Start();
+            UnityEditor.AssemblyReloadEvents.beforeAssemblyReload += Stop;
+            // UnityEditor.AssemblyReloadEvents.beforeAssemblyReload += () => { NetMQConfig.Cleanup(); };
+        }
+
+        private void RunSocket()
+        {
+            try
+            {
+                AsyncIO.ForceDotNet.Force();
+                using (var socket = new RequestSocket(address))
+                {
+                    Debug.Log("address: " + address);
+                    socket.SendFrame("Hello!");
+                    Debug.Log("SendFrame called.");
+                    string msg;
+                    bool success = socket.TryReceiveFrameString(TimeSpan.FromSeconds(10), out msg);
+                    Debug.Log($"ReceiveFrame called. success: {success}, msg: {msg}");
+                }
+                NetMQConfig.Cleanup();
+                UnityEditor.AssemblyReloadEvents.beforeAssemblyReload -= Stop;
+                // fucking compatibly of Unity and NetMQ , but fixed now!
+            }
+            catch(TerminatingException)
+            {
+                Debug.Log("socket terminated.");
+            }
+        }
+
+        [ContextMenu("Stop")]
+        public void Stop()
+        {
+            NetMQConfig.Cleanup(false);
+            Debug.Log("Clean up.");
+            UnityEditor.AssemblyReloadEvents.beforeAssemblyReload -= Stop;
+            Debug.Log("Cleaned reloadEvent");
         }
     } 
 }
