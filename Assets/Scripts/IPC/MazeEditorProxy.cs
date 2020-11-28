fe7a5844f4d9cb82475256b3600c26bcb140fdf2
Hyt <69683382@qq.com>
2020-11-28 11:36:49 +0800

    成功建立与服务器的联系
    下一步是处理相关事件

---
+++ Assets/Scripts/IPC/MazeEditorProxy.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading;
+using NetMQ;
+using NetMQ.Sockets;
+using UnityEngine;
+
+public class MazeEditorProxy : MonoBehaviour
+{
+    public enum MazeEditorEventType
+    {
+        /// <summary>
+        /// 握手
+        /// </summary>
+        HandShake,
+        /// <summary>
+        /// 更新迷宫
+        /// </summary>
+        Update,
+        /// <summary>
+        /// 迷宫文件路径更新
+        /// </summary>
+        MazePathChange,
+        /// <summary>
+        /// 结果路径更新
+        /// </summary>
+        ResultPathChange,
+        /// <summary>
+        /// 未知类型
+        /// </summary>
+        Unknown
+    }
+    public struct MazeEditorEvent
+    {
+        public MazeEditorEventType eventType;
+        public string data;
+        public MazeEditorEvent(MazeEditorEventType eventType, string data)
+        {
+            this.eventType = eventType;
+            this.data = data;
+        }
+    }
+    private RequestSocket socket;
+    private Thread sockeThread;
+    private bool terminated = false;
+
+    private ConcurrentQueue<MazeEditorEvent> eventQueue = new ConcurrentQueue<MazeEditorEvent>();
+
+    public static MazeEditorProxy instance = null;
+    public event Action<MazeEditorEvent> editorHandShakeEvent;
+    public event Action<MazeEditorEvent> mazeUpdateEvent;
+    public event Action<MazeEditorEvent> mazePathChangeEvent;
+    public event Action<MazeEditorEvent> resultPathChangeEvent;
+
+    private void Awake()
+    {
+        // 建立单例
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Debug.Log("Cannot have duplicated MazeEditorProxy in one scene!");
+            return;
+        }
+        // 初始化AsyncIO
+        AsyncIO.ForceDotNet.Force();
+        // 建立socket并启动监听线程
+        // TODO: 配置项: 连接的目标地址
+        socket = new RequestSocket(">tcp://localhost:25565");
+        sockeThread = new Thread(SocketLoop);
+        sockeThread.Start();
+
+    }
+
+    private void Update()
+    {
+        MazeEditorEvent editorEvent;
+        while(eventQueue.TryDequeue(out editorEvent))
+        {
+            switch(editorEvent.eventType)
+            {
+                case MazeEditorEventType.HandShake:
+                    mazeUpdateEvent?.Invoke(editorEvent);
+                    break;
+                case MazeEditorEventType.Update:
+                    mazeUpdateEvent?.Invoke(editorEvent);
+                    break;
+                case MazeEditorEventType.MazePathChange:
+                    mazePathChangeEvent?.Invoke(editorEvent);
+                    break;
+                case MazeEditorEventType.ResultPathChange:
+                    resultPathChangeEvent?.Invoke(editorEvent);
+                    break;
+                case MazeEditorEventType.Unknown:
+                    Debug.LogError("Unknown msg!");
+                    break;
+            }
+        }
+    }
+
+    private void OnApplicationQuit()
+    {
+        ClearSocket();
+    }
+
+    /// <summary>
+    /// 解析接收数据
+    /// </summary>
+    private void ParseFrameString(string toParse)
+    {
+        string head;
+        string data;
+        int indexOfData = toParse.IndexOf(' ') + 1;
+        if(indexOfData > 0)
+        {
+            head = toParse.Substring(0, indexOfData - 1);
+            data = toParse.Substring(indexOfData);
+        }
+        else
+        {
+            head = toParse;
+            data = "";
+        }
+
+        MazeEditorEventType eventType;
+        switch(head)
+        {
+            case "Maze_Server":
+                eventType = MazeEditorEventType.HandShake;
+                break;
+            case "Request_update":
+                eventType = MazeEditorEventType.Update;
+                break;
+            case "path_maze":
+                eventType = MazeEditorEventType.MazePathChange;
+                break;
+            case "answer":
+                eventType = MazeEditorEventType.ResultPathChange;
+                break;
+            default:
+                Debug.LogError($"Unknown head! head: {head}, data: {data}");
+                eventType = MazeEditorEventType.Unknown;
+                break;
+        }
+
+        eventQueue.Enqueue(new MazeEditorEvent(eventType, data));
+    }
+
+    /// <summary>
+    /// 套接字循环, 在背景线程中运行
+    /// </summary>
+    private void SocketLoop()
+    {
+        try
+        {
+            socket.SendFrame("Unity_Client");
+            ParseFrameString(socket.ReceiveFrameString());
+            while (!terminated)
+            {
+                socket.SendFrameEmpty();
+                ParseFrameString(socket.ReceiveFrameString());
+            }
+        }
+        catch (TerminatingException)
+        {
+            Debug.Log("Socket terminated.");
+            NetMQConfig.Cleanup();
+        }
+        catch(Exception e)
+        {
+            Debug.LogError("Oops! something wrong with exception " + e);
+            NetMQConfig.Cleanup();
+        }
+    }
+
+    /// <summary>
+    /// 清除所有由于使用socket产生的资源
+    /// </summary>
+    private void ClearSocket()
+    {
+        socket.Close();
+        NetMQConfig.Cleanup();
+    }
+
+    protected virtual void OnMazeUpdateEvent()
+    {
+    }
+}
--- Assets/Scripts/Tester.cs
+++ Assets/Scripts/Tester.cs
@@ -13,9 +13,11 @@ namespace MazeViewer
     /// </summary>
     public class Tester : MonoBehaviour
     {
-        private Thread thread;
-
-        public string address;
+        private void Start()
+        {
+            MazeEditorProxy.instance.editorHandShakeEvent += e => { Debug.Log("Hand shake!"); };
+            MazeEditorProxy.instance.mazePathChangeEvent += e => { Debug.Log($"Maze path changed! path: {e.data}"); };
+        }
 
         //[ContextMenu("Close Socket")]
         //public void CloseSocket()
@@ -26,47 +28,57 @@ namespace MazeViewer
         //    task.Wait();
         //}
 
-        [ContextMenu("Test")]
-        public void Test()
-        {
-            thread = new Thread(RunSocket);
-            thread.IsBackground = true;
-            thread.Start();
-            UnityEditor.AssemblyReloadEvents.beforeAssemblyReload += Stop;
-            // UnityEditor.AssemblyReloadEvents.beforeAssemblyReload += () => { NetMQConfig.Cleanup(); };
-        }
+        //[ContextMenu("Test")]
+        //public void Test()
+        //{
+        //    thread = new Thread(RunSocket);
+        //    thread.IsBackground = true;
+        //    thread.Start();
+        //    UnityEditor.AssemblyReloadEvents.beforeAssemblyReload += Stop;
+        //    // UnityEditor.AssemblyReloadEvents.beforeAssemblyReload += () => { NetMQConfig.Cleanup(); };
+        //}
 
-        private void RunSocket()
-        {
-            try
-            {
-                AsyncIO.ForceDotNet.Force();
-                using (var socket = new RequestSocket(address))
-                {
-                    Debug.Log("address: " + address);
-                    socket.SendFrame("Hello!");
-                    Debug.Log("SendFrame called.");
-                    string msg;
-                    bool success = socket.TryReceiveFrameString(TimeSpan.FromSeconds(10), out msg);
-                    Debug.Log($"ReceiveFrame called. success: {success}, msg: {msg}");
-                }
-                NetMQConfig.Cleanup();
-                UnityEditor.AssemblyReloadEvents.beforeAssemblyReload -= Stop;
-                // fucking compatibly of Unity and NetMQ , but fixed now!
-            }
-            catch(TerminatingException)
-            {
-                Debug.Log("socket terminated.");
-            }
-        }
+        //private void RunSocket()
+        //{
+        //    try
+        //    {
+        //        AsyncIO.ForceDotNet.Force();
+        //        using (var socket = new RequestSocket(address))
+        //        {
+        //            Debug.Log("address: " + address);
+        //            socket.SendFrame("Hello!");
+        //            Debug.Log("SendFrame called.");
+        //            string msg;
+        //            bool success = socket.TryReceiveFrameString(TimeSpan.FromSeconds(10), out msg);
+        //            Debug.Log($"ReceiveFrame called. success: {success}, msg: {msg}");
+        //        }
+        //        NetMQConfig.Cleanup();
+        //        UnityEditor.AssemblyReloadEvents.beforeAssemblyReload -= Stop;
+        //        // fucking compatibly of Unity and NetMQ , but fixed now!
+        //    }
+        //    catch(TerminatingException)
+        //    {
+        //        Debug.Log("socket terminated.");
+        //    }
+        //}
 
-        [ContextMenu("Stop")]
-        public void Stop()
-        {
-            NetMQConfig.Cleanup(false);
-            Debug.Log("Clean up.");
-            UnityEditor.AssemblyReloadEvents.beforeAssemblyReload -= Stop;
-            Debug.Log("Cleaned reloadEvent");
-        }
+        //private void OnApplicationQuit()
+        //{
+        //    Debug.Log("On Application Quit");
+        //}
+
+        //private void OnDisable()
+        //{
+        //    Debug.Log("On Disable");
+        //}
+
+        //[ContextMenu("Stop")]
+        //public void Stop()
+        //{
+        //    NetMQConfig.Cleanup(false);
+        //    Debug.Log("Clean up.");
+        //    UnityEditor.AssemblyReloadEvents.beforeAssemblyReload -= Stop;
+        //    Debug.Log("Cleaned reloadEvent");
+        //}
     } 
 }
