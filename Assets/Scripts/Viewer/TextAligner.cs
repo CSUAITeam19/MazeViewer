5fc9fc9eadef68748d4fa487e113e00c96e829e3
Hyt <69683382@qq.com>
2020-12-12 00:32:15 +0800

    实现了按距离剔除文本, 大迷宫场景性能大幅增长
    实际上刚好能看到一两格文本的距离并不能看清文字, 而剔除掉大部分文字可以使帧率几乎翻倍

--- Assets/Scripts/Viewer/TextAligner.cs
+++ Assets/Scripts/Viewer/TextAligner.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,7 +8,19 @@ namespace MazeViewer.Viewer
     public class TextAligner : MonoBehaviour
     {
         [SerializeField] private Transform anchor = default;
-        
+        [SerializeField] private int availableDistance = 10;
+        private MeshRenderer[] renderers;
+        /// <summary>
+        /// 到摄像头的曼哈顿距离
+        /// </summary>
+        private int DistanceToCamera
+        {
+            get
+            {
+                var delta = transform.position - Camera.main.transform.position;
+                return (int) (Mathf.Abs(delta.x) + Mathf.Abs(delta.y) + Mathf.Abs(delta.z));
+            }
+        }
         /// <summary>
         /// 获取要对齐的方向
         /// </summary>
@@ -25,12 +38,31 @@ namespace MazeViewer.Viewer
             }
         }
 
+        private void Awake()
+        {
+            renderers = GetComponentsInChildren<MeshRenderer>();
+        }
+
         [ExecuteInEditMode]
         private void Update()
         {
-            Vector3 current = anchor.forward;
-            Quaternion rotate = Quaternion.FromToRotation(current,ToAlignDirection());
-            transform.rotation *= rotate;
+            if(DistanceToCamera <= availableDistance)
+            {
+                foreach (var meshRenderer in renderers)
+                {
+                    meshRenderer.enabled = true;
+                }
+                Vector3 current = anchor.forward;
+                Quaternion rotate = Quaternion.FromToRotation(current, ToAlignDirection());
+                transform.rotation *= rotate;
+            }
+            else
+            {
+                foreach (var meshRenderer in renderers)
+                {
+                    meshRenderer.enabled = false;
+                }
+            }
         }
     }
 
